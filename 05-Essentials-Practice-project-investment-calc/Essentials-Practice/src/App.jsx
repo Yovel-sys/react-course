33b9331987f58126301f313861ff16d3a5f0e33f
Yovel <yovel-am@drornet.org.il>
2025-10-31 16:46:13 +0200

    4

--- 05-Essentials-Practice-project-investment-calc/Essentials-Practice/src/App.jsx
+++ 05-Essentials-Practice-project-investment-calc/Essentials-Practice/src/App.jsx
@@ -11,11 +11,13 @@ function App() {
     duration: 10,
   });
 
+  const inputIsValid = userInput.duration >= 1;
+
   const handleChange = (inputId, newValue) => {
     setUserInput((prevUserInput) => {
       return {
         ...prevUserInput,
-        [inputId]: newValue,
+        [inputId]: +newValue,
       };
     });
   };
@@ -24,7 +26,10 @@ function App() {
     <>
       <Header />
       <UserInput userInput={userInput} onChange={handleChange} />
-      <Results input={userInput} />
+      {!inputIsValid && (
+        <p className="center">Please enter a duration greater then zero</p>
+      )}
+      {inputIsValid && <Results input={userInput} />}
     </>
   );
 }
--- 05-Essentials-Practice-project-investment-calc/Essentials-Practice/src/components/Results.jsx
+++ 05-Essentials-Practice-project-investment-calc/Essentials-Practice/src/components/Results.jsx
@@ -1,38 +1,35 @@
-import { calculateInvestmentResults } from "../util/investment.js";
+import { calculateInvestmentResults, formatter } from "../util/investment.js";
 export default function Results({ input }) {
-  const investmentResult = calculateInvestmentResults(input);
-  console.log(investmentResult);
+  const resultData = calculateInvestmentResults(input);
   let interestUntilNow = 0;
-  let totalInvestment = Number(input.initialInvestment);
+  let totalInvestment = input.initialInvestment;
 
   return (
-    <div id="result">
-      <table>
-        <thead>
-          <tr>
-            <th>Year</th>
-            <th>Investment Value</th>
-            <th>Interest (Year)</th>
-            <th>Total Interest</th>
-            <th>Invested Capital</th>
-          </tr>
-        </thead>
-        <tbody>
-          {investmentResult.map((row) => {
-            interestUntilNow += row.interest;
-            totalInvestment += input.annualInvestment;
-            return (
-              <tr key={row.year}>
-                <th>{row.year}</th>
-                <td>{row.valueEndOfYear}</td>
-                <td>{row.interest}</td>
-                <td>{interestUntilNow}</td>
-                <td>{totalInvestment}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {resultData.map((YearData) => {
+          interestUntilNow += YearData.interest;
+          totalInvestment += input.annualInvestment;
+          return (
+            <tr key={YearData.year}>
+              <th>{YearData.year}</th>
+              <td>{formatter.format(YearData.valueEndOfYear)}</td>
+              <td>{formatter.format(YearData.interest)}</td>
+              <td>{formatter.format(interestUntilNow)}</td>
+              <td>{formatter.format(totalInvestment)}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 }
